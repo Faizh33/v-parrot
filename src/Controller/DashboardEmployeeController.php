a3cea1ec467d73bd48b2d7e1e16f6724f47c089b
Erika REDON <erika_redon@hotmail.com>
2023-08-03 16:21:17 +0200

    creating dashboard pages

---
+++ src/Controller/DashboardEmployeeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardEmployeeController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord-employe", name="app_dashboard_employee")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard_employee/index.html.twig');
+    }
+}
