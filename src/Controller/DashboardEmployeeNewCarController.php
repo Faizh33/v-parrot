2911c64d06ab057fc76e28432fbbf4cf41474a6a
Erika REDON <erika_redon@hotmail.com>
2023-08-04 13:35:21 +0200

    creating new car page

---
+++ src/Controller/DashboardEmployeeNewCarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardEmployeeNewCarController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord-employe/nouvelle-voiture", name="app_dashboard_employee_new_car")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard_employee_new_car/index.html.twig');
+    }
+}
