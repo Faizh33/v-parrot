827a314809ad91caae1154ca3bdbe189f5e60283
Erika REDON <erika_redon@hotmail.com>
2023-08-07 12:15:58 +0200

    creation of the review moderation page

---
+++ src/Controller/DashboardEmployeeReviewsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardEmployeeReviewsController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord-employe/moderer-les-avis", name="app_dashboard_employee_reviews")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard_employee_reviews/index.html.twig');
+    }
+}
