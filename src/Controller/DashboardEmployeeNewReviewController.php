f17fa9170ccdf5dddf74f7223cb3d805111ca1b5
Erika REDON <erika_redon@hotmail.com>
2023-08-07 11:49:07 +0200

    create new review form for employee

---
+++ src/Controller/DashboardEmployeeNewReviewController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardEmployeeNewReviewController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord-employe/ajouter-un-avis", name="app_dashboard_employee_new_review")
+     */
+    public function index(): Response
+    {
+        return $this->render('dashboard_employee_new_review/index.html.twig', [
+            'controller_name' => 'DashboardEmployeeNewReviewController',
+        ]);
+    }
+}
