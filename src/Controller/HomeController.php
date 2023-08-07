896aa300c0c13c080ba44a5e5a0c5ed664e70561
Erika REDON <erika_redon@hotmail.com>
2023-08-07 16:30:07 +0200

    recuperate repair datas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\RepairRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $repairRepository;
+
+    public function __construct(RepairRepository $repairRepository)
+    {
+        $this->repairRepository = $repairRepository;
+    }
+
     /**
      * @Route("/home", name="app_home")
      */
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        $repairs = $this->repairRepository->findAll();
+
+        return $this->render('home.html.twig', [
+            'repairs' => $repairs,
+        ]);
     }
 }
