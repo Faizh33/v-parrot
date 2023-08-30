beb0d36a12142f3e2fa252cf1f80c28a044e5cc8
Erika REDON <erika_redon@hotmail.com>
2023-08-30 13:12:01 +0200

    sends reviews to the TemporaryReview table and displays reviews from the Review table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,29 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Entity\TemporaryReview;
+use App\Form\TemporaryReviewType;
 use App\Repository\RepairRepository;
+use App\Repository\ReviewRepository;
+use App\Repository\TemporaryReviewRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     private $repairRepository;
+    private $temporaryReviewRepository;
+    private $reviewRepository;
+    private $entityManager;
 
-    public function __construct(RepairRepository $repairRepository)
+    public function __construct(RepairRepository $repairRepository, TemporaryReviewRepository $temporaryReviewRepository, ReviewRepository $reviewRepository, EntityManagerInterface $entityManager)
     {
         $this->repairRepository = $repairRepository;
+        $this->temporaryReviewRepository = $temporaryReviewRepository;
+        $this->reviewRepository = $reviewRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/home", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $temporaryReview = new TemporaryReview();
+        $form = $this->createForm(TemporaryReviewType::class, $temporaryReview);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedRate = $request->request->get('rating');
+            $temporaryReview->setRate($selectedRate);
+
+            $this->entityManager->persist($temporaryReview);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
         $repairs = $this->repairRepository->findAll();
+        $reviews = $this->reviewRepository->findAll();
 
         return $this->render('home.html.twig', [
             'repairs' => $repairs,
+            'reviews' => $reviews,
+            'form' => $form->createView(),
         ]);
     }
 }
