655a2131cb54a43a7260592616c6ebbb1ee09a17
Erika REDON <erika_redon@hotmail.com>
2023-09-07 10:48:51 +0200

    insertion/retrieval of garage status data and deletion of old data in the database each time it is added

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -2,17 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\GarageState;
+use App\Repository\GarageStateRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardAdminController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/dashboard-admin-1", name="app_dashboard_admin")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('dashboard_admin.html.twig');
+        if ($request->isMethod('POST')) {
+            if ($request->request->has('ouvert')) {
+                $status = 'ouvert';
+                $this->deleteOldGarageStates();
+            } elseif ($request->request->has('fermer')) {
+                $status = 'fermé';
+                $this->deleteOldGarageStates();
+            } else {
+                $status = '{statut inconnu}';
+                $this->deleteOldGarageStates();
+            }
+
+            $garageState = new GarageState();
+            $garageState->setStatus($status);
+
+            $this->entityManager->persist($garageState);
+            $this->entityManager->flush();
+        }
+
+        $repository = $this->entityManager->getRepository(GarageState::class);
+        $status = $repository->find(1)->getStatus();
+
+        return $this->render('dashboard_admin.html.twig', [
+            'status' => $status,
+        ]);
+    }
+
+    private function deleteOldGarageStates(): void
+    {
+        // Obtenez le référentiel des anciennes données de GarageState
+        $garageStateRepository = $this->entityManager->getRepository(GarageState::class);
+
+        // Supprimez toutes les anciennes données
+        $garageStateRepository->createQueryBuilder('g')
+            ->delete()
+            ->getQuery()
+            ->execute();
+
+        $query = 'ALTER TABLE garage_state AUTO_INCREMENT = 1';
+        $connection = $this->entityManager->getConnection();
+        $connection->executeStatement($query);
     }
 }
+
