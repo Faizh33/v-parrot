9ce617751927889c7e2a9dce5ad95b39b1f380e5
Erika REDON <erika_redon@hotmail.com>
2023-09-07 17:42:49 +0200

    edited builder to use twig form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,8 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,12 +14,18 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-control', 'id' => 'email', 'required' => 'required'],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-control', 'id' => 'password', 'required' => 'required'],
+            ])
         ;
-    }
+    }    
 
     public function configureOptions(OptionsResolver $resolver): void
     {
