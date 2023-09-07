6bf768854939f210285239914cea463ac26f98b3
Erika REDON <erika_redon@hotmail.com>
2023-09-07 10:46:55 +0200

    retrieval of garage status data

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\GarageState;
+use App\Repository\GarageStateRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ContactController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/contact", name="app_contact")
      */
     public function index(): Response
     {
-        return $this->render('contact.html.twig');
+        $repository = $this->entityManager->getRepository(GarageState::class);
+        $status = $repository->find(1)->getStatus();
+
+        return $this->render('contact.html.twig', [
+            'status' => $status,
+        ]);
     }
 }
