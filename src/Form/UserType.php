a2147e54af17a31855d67dbce82739dcbd3e80cb
Erika REDON <erika_redon@hotmail.com>
2023-09-12 14:13:44 +0200

    adding new properties to the builder

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,14 +7,24 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-control', 'id' => 'email', 'required' => 'required'],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'label_attr' => ['class' => 'label'],
+                'attr' => ['class' => 'form-control', 'id' => 'password', 'required' => 'required'],
+            ])
             ->add('firstName')
             ->add('lastName')
         ;
