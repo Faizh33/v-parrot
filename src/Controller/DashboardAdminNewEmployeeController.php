6bcdf0020d2f206e296bf3657a1f05b81202ea08
Erika REDON <erika_redon@hotmail.com>
2023-09-12 14:15:43 +0200

    adding authentication logic

--- src/Controller/DashboardAdminNewEmployeeController.php
+++ src/Controller/DashboardAdminNewEmployeeController.php
@@ -4,40 +4,45 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Security\AppCustomAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class DashboardAdminNewEmployeeController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-
     /**
      * @Route("/tableau-de-bord-admin/nouvel-employe", name="app_dashboard_admin_new_employee")
      */
-    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {        
         $employee = new User();
-        $employee->setRoles(['ROLE_USER']);
         $form = $this->createForm(UserType::class, $employee);
-
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $passwordHasher->hashPassword($employee, $employee->getPassword());
-            $employee->setPassword($hashedPassword);
 
-            $this->entityManager->persist($employee);
-            $this->entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $employee->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $employee,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $employee->setRoles(['ROLE_USER']);
+            $entityManager->persist($employee);
+            $entityManager->flush();
 
             return $this->redirectToRoute('dashboard-admin-1');
+
+            return $userAuthenticator->authenticateUser(
+                $employee,
+                $authenticator,
+                $request
+            );
         }
 
         return $this->render('dashboard_admin_new_employee.html.twig', [
