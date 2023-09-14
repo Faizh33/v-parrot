16e06156600bb994cf8d084486adaf627495785e
Erika REDON <erika_redon@hotmail.com>
2023-09-14 09:35:44 +0200

    removing bootstrap includes & added logic to hide the navbar logos when the menu is open

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var iconsToHide = document.querySelectorAll('.navbar-person, .navbar-telephone');
+
+    navbarToggler.addEventListener('click', function () {
+        for (var i = 0; i < iconsToHide.length; i++) {
+            iconsToHide[i].classList.toggle('hidden');
+        }
+    });
+});
